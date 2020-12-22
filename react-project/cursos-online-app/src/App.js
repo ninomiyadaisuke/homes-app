b4b52f11e5e4ed6708ca77511cab770934fb2494
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-22 14:04:41 +0900

    Get countries name using axios

--- react-project/cursos-online-app/src/App.js
+++ react-project/cursos-online-app/src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from "react"
+import Profile from "./components/Profile"
+
 
 function App() {
+  const [name, setName] = useState("No name")
+  
+  const eventBoxText = (e) => {
+    setName(e.target.value)
+  }
+
   return (
     <div>
-      <h1>ハロー</h1>
-      <input name="nombre" type="text"/>
+      <h1>ハロー{name}</h1>
+      <input name="name" type="text" value={name} onChange={eventBoxText} />
+      <Profile />
     </div>
   );
 }
