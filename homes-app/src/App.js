3ecc9be017172d0582221b2070a860da321eb613
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-23 10:55:29 +0900

    section4 22

--- homes-app/src/App.js
+++ homes-app/src/App.js
@@ -6,7 +6,7 @@ import AppNavbar from "./components/layout/AppNavbar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider"
 import theme from "./theme/theme"
-
+import UserRegistration from "./components/security/UserRegistration"
 
 class App extends Component{
   render() {
@@ -18,6 +18,7 @@ class App extends Component{
           <Grid container>
             <Switch>
               <Route path="/" exact component={ListaInmuebles}></Route>
+              <Route path="/auth/userregistration" exact component={UserRegistration}></Route>
             </Switch>
           </Grid>
           
