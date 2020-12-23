d163224f1446c4f85d2a2312a8e880d8973b874d
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-23 13:43:04 +0900

    Firebase instance creation in the draft

---
+++ homes-app/src/server/firebase.js
@@ -0,0 +1,21 @@
+import app from "firebase/app"
+
+const config = {
+  apiKey: "AIzaSyB0m8dbvVeoauF94Z1eqJWOXklECG0Kl80",
+  authDomain: "home-7400a.firebaseapp.com",
+  projectId: "home-7400a",
+  storageBucket: "home-7400a.appspot.com",
+  messagingSenderId: "883517423838",
+  appId: "1:883517423838:web:aa82ba8bc2b2e4d986424f",
+  measurementId: "G-VVEWME7C5F"
+};
+
+class Firebase{
+  constructor() {
+    app.initializeApp(config)
+    this.db = app.firestore()
+  }
+
+}
+
+export default Firebase
