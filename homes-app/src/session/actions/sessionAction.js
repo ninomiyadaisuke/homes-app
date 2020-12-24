2f4868373c4db8cd8923cc7450d55856b28871f5
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-24 11:32:25 +0900

    create react Hooks - Actions Part  2

--- homes-app/src/session/actions/sessionAction.js
+++ homes-app/src/session/actions/sessionAction.js
@@ -9,8 +9,42 @@ export const startSession = (dispatch, firebase, email, password) => {
           .get()
           .then(doc => {
             const userDB = doc.data()
-            
+            dispatch({
+              type: "START_SESSION",
+              session: userDB,
+              authenticated: true
+            })
+            resolve()
           })
       }) 
+      .catch(error => {
+        console.log("error", error);
+      })
+  })
+}
+
+export const createUser = (dispatch, firebase, user) => {
+  return new Promise((resolve, eject) => {
+    firebase.auth
+      .createUserWithEmailAndPassword(user.email, user.password)
+      .then(auth => {
+        firebase.db
+          .collection("Users")
+          .doc(auth.user.uid)
+          .set({
+            id: auth.user.uid,
+            email: user.email,
+            name: user.name,
+            lastName: user.lastName
+          }, { maege: true })
+          .then(doc => {
+            user.id = auth.user.uid
+            dispatch({
+              type: "START_SESSION",
+              session: user,
+              authenticated: true
+            })
+          })
+      })
   })
 }
